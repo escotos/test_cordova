ac3058961003cbe24e303591fbdd67e80c1e3f18
Larry Nickerson <lnickers@us.ibm.com>
2015-08-21 17:32:11 -0500

    added unit tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,13 @@
 exports.defineAutoTests = function () {
 	describe('MFPCore tests', function () {
 
+		it('should exist', function(){
+			expect(MFPClient).toBeDefined();
+		});
+
+		// this test is expected to fail, we did it for fun!!!
+		xit('should not exist', function(){
+			expect(MFPClient).not.toBeDefined();
+		});
 	});
-}
+};

6469847b5ace143fd9190a66539b28b0e046e337
escotos <escotos@us.ibm.com>
2015-09-10 17:59:24 -0500

    (WIP): Handling Requests in callbacks

--- src/android/MFPResourceRequest.java
+++ src/android/MFPResourceRequest.java
@@ -2,16 +2,10 @@ package com.ibm.mobilefirstplatform.clientsdk.cordovaplugins.core;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
-import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
-import com.ibm.mobilefirstplatform.clientsdk.android.core.api.FailResponse;
-import com.ibm.mobilefirstplatform.clientsdk.android.core.api.MFPRequest;
-import com.ibm.mobilefirstplatform.clientsdk.android.core.api.ResourceRequest;
-import com.ibm.mobilefirstplatform.clientsdk.android.core.api.Response;
-import com.ibm.mobilefirstplatform.clientsdk.android.core.api.ResponseListener;
-import com.ibm.mobilefirstplatform.clientsdk.android.security.api.AuthenticationContext;
-import com.ibm.mobilefirstplatform.clientsdk.android.security.api.AuthenticationListener;
-import com.ibm.mobilefirstplatform.clientsdk.android.security.api.AuthorizationManager;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.*;
+import com.ibm.mobilefirstplatform.clientsdk.android.security.api.*;
 import com.ibm.mobilefirstplatform.clientsdk.android.logger.api.Logger;
 
 import android.util.Log;
@@ -46,31 +40,40 @@ public class MFPResourceRequest extends CordovaPlugin {
         return false;
     }
 
-    //TODO: Refactor
+    //TODO: Refactor send & sendFormParameters
     public void send(JSONArray args, final CallbackContext callbackContext) throws JSONException {
         JSONObject myrequest = args.getJSONObject(0);
         Log.d(TAG, "myrequest.toString() = " + myrequest.toString());
         try {
             final MFPRequest nativeRequest = unpackRequest(myrequest);
-            final String bodyText = myrequest.getString("body");
+            final String bodyText = (myrequest.getString("body") != null) ? myrequest.getString("body") : "";
             printNativeRequest(nativeRequest);
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
                     nativeRequest.send(bodyText, new ResponseListener() {
                         @Override
                         public void onSuccess(Response response) {
-                            Log.d(TAG, "Success: " + response.getResponseText());
-                            callbackContext.success("Native Success");
+                            try {
+                                PluginResult result = new PluginResult(PluginResult.Status.OK, packResponse(response));
+                                result.setKeepCallback(true);
+                                Log.d(TAG, "Success = Sending plugin result to javascript");
+                                callbackContext.sendPluginResult(result);
+                            } catch (JSONException e) { e.printStackTrace(); }
                         }
                         @Override
                         public void onFailure(FailResponse failResponse, Throwable throwable) {
-                            Log.d(TAG, "Failure: " + failResponse.getStatus());
-                            callbackContext.error("Native Error.");
+                            try {
+                                PluginResult result = new PluginResult(PluginResult.Status.ERROR, packResponse(failResponse));
+                                result.setKeepCallback(true);
+                                Log.d(TAG, "Failure = Sending plugin result to javascript");
+                                callbackContext.sendPluginResult(result);
+                            } catch (JSONException e) { e.printStackTrace(); }
                         }
                     });
                 }
             });
-        } catch (MalformedURLException e) { e.printStackTrace(); }
+        }
+        catch (MalformedURLException e) { Log.d(TAG, "Malformed URL Exception"); e.printStackTrace(); }
     }
 
     //TODO: Refactor
@@ -79,53 +82,101 @@ public class MFPResourceRequest extends CordovaPlugin {
         Log.d(TAG, "myrequest.toString() = " + myrequest.toString());
         try {
             final MFPRequest nativeRequest = unpackRequest(myrequest);
-            final Map<String, String> formParameters = toNativeMap(myrequest.getJSONObject("body"));
+            final Map<String, String> formParameters = fromJSONtoHashMap(myrequest.getJSONObject("body"));
+            Log.d(TAG, "BODY " + myrequest.getJSONObject("body").toString());
             printNativeRequest(nativeRequest);
+
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
                     nativeRequest.send(formParameters, new ResponseListener() {
                         @Override
                         public void onSuccess(Response response) {
                             Log.d(TAG, "Success: " + response.getResponseText());
-                            callbackContext.success("Native Success");
+                            //callbackContext.success("Native Success");
+                            try {
+                                PluginResult result = new PluginResult(PluginResult.Status.OK, packResponse(response));
+                                result.setKeepCallback(true);
+                                callbackContext.sendPluginResult(result);
+                            } catch (JSONException e) { e.printStackTrace(); }
                         }
                         @Override
                         public void onFailure(FailResponse failResponse, Throwable throwable) {
                             Log.d(TAG, "Failure: " + failResponse.getStatus());
-                            callbackContext.error("Native Error.");
+                            //callbackContext.error("Native Error.");
+                            try {
+                                PluginResult result = new PluginResult(PluginResult.Status.ERROR, packResponse(failResponse));
+                                result.setKeepCallback(true);
+                                callbackContext.sendPluginResult(result);
+                            } catch (JSONException e) { e.printStackTrace(); }
                         }
                     });
                 }
             });
-        } catch (MalformedURLException e) { e.printStackTrace(); }
+        }
+        catch (MalformedURLException e) { Log.d(TAG, "Malformed URL Exception"); e.printStackTrace(); }
     }
 
+
+    //TODO : Refactor
     private MFPRequest unpackRequest(JSONObject jsRequest) throws JSONException {
         //Parse the request from Javascript
         String url    = jsRequest.getString("url");
         String method = jsRequest.getString("method");
         int timeout   = jsRequest.getInt("timeout");
-        Map<String, List<String>> headers   = toNativeMap(jsRequest.getJSONObject("headers"));
-        Map<String, String> queryParameters = toNativeMap(jsRequest.getJSONObject("queryParameters"));
+        Map<String, List<String>> headers = null;
+        Map<String, String> queryParameters = null;
+
+        if (jsRequest.getJSONObject("headers") != null)
+            headers = fromJSONtoHashMap(jsRequest.getJSONObject("headers"));
+        if (jsRequest.getJSONObject("queryParameters") != null)
+            queryParameters = fromJSONtoHashMap(jsRequest.getJSONObject("queryParameters"));
 
         //Build the request using the native Android SDK
         MFPRequest nativeRequest = null;
         try {
             nativeRequest = new MFPRequest(url, method);
             nativeRequest.setTimeout(timeout);
-            nativeRequest.setHeaders(headers);
-            nativeRequest.setQueryParameters(queryParameters);
+            if (headers != null)
+                nativeRequest.setHeaders(headers);
+            if (queryParameters != null)
+                nativeRequest.setQueryParameters(queryParameters);
         } catch (MalformedURLException e) { e.printStackTrace(); }
         return nativeRequest;
     }
 
-    private static Map toNativeMap(JSONObject originalJSON) throws JSONException {
+    private String packResponse(Response response) throws JSONException {
+        JSONObject jsonResponse = new JSONObject();
+
+        jsonResponse.put("httpStatus", response.getStatus());
+        if (response.getResponseText() != null) {
+            jsonResponse.put("responseText", response.getResponseText());
+        }
+        if (response.getResponseJSON() != null) {
+            jsonResponse.put("responseJSON", response.getResponseJSON().toString());
+        }
+        if (response.getResponseHeaders() != null) {
+            Log.d(TAG, "packResponse: Received Response Headers");
+            JSONObject jsonHeaders = new JSONObject(response.getResponseHeaders());
+            Log.d(TAG, "packResponse headers to string: " + jsonHeaders.toString());
+            jsonResponse.put("headers", jsonHeaders.toString());
+        }
+
+        if(response instanceof FailResponse) {
+            jsonResponse.put("errorCode", response.getStatus());
+            jsonResponse.put("errorDescription", response.getStatus());
+        }
+
+        Log.d(TAG, "packResponse final JSON: " + jsonResponse.toString());
+        return jsonResponse.toString();
+    }
+
+    private static Map fromJSONtoHashMap(JSONObject originalJSON) throws JSONException {
         Map<String, Object> convertedMap = new HashMap<String, Object>();
 
         Iterator<?> keys = originalJSON.keys();
         while(keys.hasNext()) {
             String key = (String)keys.next();
-            // Handle "key" => [array]
+            // Handle "key" => [array of Strings]
             if(originalJSON.get(key) instanceof JSONArray) {
                 JSONArray headerValues = originalJSON.getJSONArray(key);
 
@@ -133,10 +184,9 @@ public class MFPResourceRequest extends CordovaPlugin {
                 for(int i=0; i < headerValues.length(); i++) {
                     listedHeaderValues.add(headerValues.getString(i));
                 }
-
                 convertedMap.put(key, listedHeaderValues);
             }
-            // Handle "key" => "string value"
+            // Handle "key" => "value (string)"
             else if (originalJSON.get(key) instanceof String) {
                 convertedMap.put(key, originalJSON.getString(key));
             }
@@ -144,9 +194,8 @@ public class MFPResourceRequest extends CordovaPlugin {
         return convertedMap;
     }
 
-
     private void printNativeRequest(MFPRequest theRequest) throws MalformedURLException {
-        Log.d(TAG, "[START] printNativeRequest()");
+        Log.d(TAG, "\n[START] printNativeRequest()");
         Log.d(TAG, "URL = \t" + theRequest.getUrl());
         Log.d(TAG, "Method = \t" + theRequest.getMethod());
         Log.d(TAG, "Timeout = \t" + theRequest.getTimeout());
@@ -155,21 +204,24 @@ public class MFPResourceRequest extends CordovaPlugin {
         Map<String, String> native_params        = theRequest.getQueryParameters();
 
         Log.d(TAG, "-----Headers-----");
-        for (String key: native_headers.keySet()){
-                    List<String> value = native_headers.get(key);
+        if (native_headers != null) {
+            for (String key: native_headers.keySet()) {
+                        List<String> value = native_headers.get(key);
 
-                    Log.d(TAG, "Header Name = " + key);
-                    for(String headerValue : value) {
-                        Log.d(TAG, "\tvalue = " + headerValue);
-                    }
+                        Log.d(TAG, "Header Name = " + key);
+                        for(String headerValue : value) {
+                            Log.d(TAG, "\tvalue = " + headerValue);
+                        }
+            }
         }
-
         Log.d(TAG, "-----Queries-----");
-        for (String key: native_params.keySet()){
-                    String value = native_params.get(key).toString();
-                    Log.d(TAG, key + " : " + value);
+        if(native_params != null) {
+            for (String key: native_params.keySet()) {
+                        String value = native_params.get(key).toString();
+                        Log.d(TAG, key + " : " + value);
+            }
         }
-        Log.d(TAG, "[END] printNativeRequest()\n");
+        Log.d(TAG, "[END] printNativeRequest()\n\n");
     }
 
 }

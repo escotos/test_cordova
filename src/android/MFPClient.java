4bda9dfccd188f4e78a28a766d4fce32fb7d643b
escotos <escotos@us.ibm.com>
2015-09-14 17:39:37 -0500

    MFPClient handles initialize method now

--- src/android/MFPClient.java
+++ src/android/MFPClient.java
@@ -32,35 +32,34 @@ public class MFPClient extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         Log.d(TAG, "In execute()");
-        Method[] methodList = this.getClass().getMethods();
-        for(Method method: methodList) {
-            System.out.println(method.toString());
+        if("initialize".equals(action)) {
+            this.initialize(args, callbackContext);
+            return true;
+        } else if("registerAuthenticationListener".equals(action)) {
+            return true;
+        } else if("unregisterAuthenticationListener".equals(action)) {
+            return true;
         }
-        return true;
+        return false;
     }
 
-    public static void initialize(JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public void initialize(JSONArray args, CallbackContext callbackContext) throws JSONException {
         String backendRoute = args.getString(0);
         String backendGuid = args.getString(1);
-        if (backendRoute != null && backendRoute.length() > 0) {
-            Log.d(TAG, "Called initialize");
-            Log.d(TAG, "Arg1: " + backendRoute);
-            Log.d(TAG, "Arg2: " + backendGuid);
-
-//             try {
-//                 BMSClient.getInstance().initialize(this.cordova.getActivity().getApplicationContext(), "http://9.148.225.106:9080", "vit1");
-//             } catch (MalformedURLException e) {
-//                 e.printStackTrace();
-//             }
-
-            Log.d(TAG, "[initialize()]: Successfully set up BMSClient!");
+        if (backendRoute != null && backendRoute.length() > 0 && backendGuid != null && backendGuid.length() > 0) {
+            try {
+                BMSClient.getInstance().initialize(this.cordova.getActivity().getApplicationContext(), backendRoute, backendGuid);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            Log.d(TAG, "initialize() => Successfully set up BMSClient!");
             callbackContext.success(backendRoute);
         } else {
-            callbackContext.error("Expected one non-empty string argument.");
+            callbackContext.error("Expected non-empty string arguments.");
         }
     }
 
-    public static void registerAuthenticationListener(JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public void registerAuthenticationListener(JSONArray args, CallbackContext callbackContext) throws JSONException {
         String realm                  = args.getString(0);
         String authenticationListener = args.getString(1);
         if (realm != null && realm.length() > 0) {
@@ -74,7 +73,7 @@ public class MFPClient extends CordovaPlugin {
         }
     }
 
-    public static void unregisterAuthenticationListener(JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public void unregisterAuthenticationListener(JSONArray args, CallbackContext callbackContext) throws JSONException {
         String authenticationListener = args.getString(0);
         if (authenticationListener != null && authenticationListener.length() > 0) {
             Log.d(TAG, "Called unregisterAuthenticationListener");

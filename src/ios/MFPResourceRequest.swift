b45325fda067ee4eac8a87658601198e84625fc9
Larry Nickerson <lnickers@austin.rr.com>
2015-09-16 09:34:49 -0500

    updated MFPResourceRequest

--- src/ios/MFPResourceRequest.swift
+++ src/ios/MFPResourceRequest.swift
@@ -28,17 +28,22 @@ import IMFCore
         // timeout
         let timeout = requestDict.objectForKey("timeout") as? Int
         nativeRequest.setTimeoutInterval(NSTimeInterval( timeout! ) )
-        
-//        // process the body
-//        if let body  = requestDict.objectForKey("body") as? NSDictionary {
-//            //let bodyData = body!.
-//            //nativeRequest.setHTTPBody(<#T##data: NSData!##NSData!#>)
-//        }
-//        else{
-//            let body = requestDict.objectForKey("body") as? String
-//            let bodyData = body!.dataUsingEncoding(NSUTF8StringEncoding)
-//            nativeRequest.setHTTPBody(bodyData)
-//        }
+
+        // process the body
+        if let body  = requestDict.objectForKey("body") as? NSDictionary {
+
+            //BEGIN LEN DEBUG
+            //let bodyData = body!.
+            //nativeRequest.setHTTPBody(data: NSData!)
+            print("!!! !!! !!! !!! BODY IS A DICTIONARY !!! !!! !!! !!!", appendNewline: false)
+            //END LEN DEBUG
+        }
+        else {
+            if let body = requestDict.objectForKey("body") as? String {
+                let bodyData = body.dataUsingEncoding(NSUTF8StringEncoding)
+                nativeRequest.setHTTPBody(bodyData)
+            }
+        }
         
         // get the headers
         let requestHeaderDict = requestDict.objectForKey("headers") as! Dictionary<String,[String]>
@@ -71,16 +76,31 @@ import IMFCore
         nativeRequest.sendWithCompletionHandler { (response: IMFResponse!, error: NSError!) -> Void in
             if (error != nil) {
                 // process the error
-                print("Error  in ViewController\(error.localizedDescription)")
                 let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: error.localizedDescription)
                 self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
             } else {
                 // process success
-                print("Response in ViewController \(response)")
-                print("Response text in ViewController \(response.responseText)")
                 let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: response.responseText)
                 self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
             }
         } // end send
-    } // end func
+    } // end func send
+
+        func sendFormParameters(command: CDVInvokedUrlCommand) {
+
+            let nativeRequest = unPackRequest(command.arguments[0] as! NSDictionary)
+
+            nativeRequest.sendWithCompletionHandler { (response: IMFResponse!, error: NSError!) -> Void in
+                if (error != nil) {
+                    // process the error
+                    let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: error.localizedDescription)
+                    self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+                } else {
+                    // process success
+                    let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: response.responseText)
+                    self.commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+                }
+            } // end send
+        } // end func sendFormParameters
+
 } // end plugin
